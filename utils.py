bbd3694ab722c9e9172c3b6e2f39159d4a6e9288
Tianci Zhong <tzhong9@gatech.edu>
2021-07-29 01:18:40 +0100

    Add save prediction in util

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 
 def augment(points, label):
     # jitter points
@@ -8,3 +10,30 @@ def augment(points, label):
     # shuffle points
     points = tf.random.shuffle(points)
     return points, label
+
+def save_prediction_image(model, test_dataset, class_map, log_dir):    
+    data = test_dataset.take(1)
+
+    points, labels = list(data)[0]
+    points = points[:8, ...]
+    labels = labels[:8, ...]
+
+    # run test data through model
+    preds = model.predict(points)
+    preds = tf.math.argmax(preds, -1)
+
+    points = points.numpy()
+
+    # plot points with predicted class and label
+    fig = plt.figure(figsize=(15, 10))
+    for i in range(8):
+        ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+        ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+        ax.set_title(
+            "pred: {:}, label: {:}".format(
+                class_map[preds[i].numpy()], class_map[labels.numpy()[i]]
+            )
+        )
+        ax.set_axis_off()
+    plt.savefig(log_dir+'prediction.png')
+    plt.close()
